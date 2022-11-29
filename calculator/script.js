12cc551f7e34e18b8773a5d1630040ec3549f34c
AsyaPolyak <asyapolyachka@gmail.com>
2022-11-29 13:59:48 +0200

    added 3 functions

--- calculator/script.js
+++ calculator/script.js
@@ -2,6 +2,18 @@
     el:'#app',
     data: {
         result:'0',
-        numbers:[1,2,3,4,5,6,7,8,9,0]
+        numbers:[1,2,3,4,5,6,7,8,9,0],
+        operations:['+','-','*','/']
+    },
+    methods:{
+        input:function(){
+
+        },
+        reset: function(){
+
+        },
+        calc: function(){
+            
+        }
     }
  });
